518f8d4b4428f9895534273ff329407b7f4ddeb6
Mateo <mateoquirosaspron@gmail.com>
2023-07-05 13:51:06 -0600

    added kwargs to base model

--- models/base_model.py
+++ models/base_model.py
@@ -6,16 +6,23 @@ from datetime import datetime
 
 class BaseModel():
     """ The base for the other classes """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initializer method """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        self.name = self.__class__.__name__
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """ This will display some text """
-        return("[{}] ({}) {}".format(type(self).__name__,
+        return("[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__))
 
     def save(self):
@@ -25,7 +32,7 @@ class BaseModel():
     def to_dict(self):
         """ Returns a dictionary with the attributes of the class """
         dictionary = self.__dict__.copy()
-        dictionary['__class__'] = self.name
+        dictionary['__class__'] = self.__class__.__name__
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
