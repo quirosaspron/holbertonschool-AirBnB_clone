47cad396a2873ac1c8b73a808624e1041822c40f
Mateo <mateoquirosaspron@gmail.com>
2023-07-05 10:32:46 -0600

    changed the str method

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,8 @@ class BaseModel():
 
     def __str__(self):
         """ This will display some text """
-        return("[{}] ({}) {}".format(self.name, self.id, self.__dict__))
+        return("[{}] ({}) {}".format(type(self).__name__,
+                                     self.id, self.__dict__))
 
     def save(self):
         """ Updates the time when modified """
